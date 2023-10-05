68956ee76995ff8de2f38445949676b66c5b468e
Sam_dev <102005672+Samilincoln@users.noreply.github.com>
2023-10-05 18:03:49 +0100

    Update settings.py

--- LittleLemon/settings.py
+++ LittleLemon/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-from decouple import config
+#from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = django-insecure-$_3wr2at2cdu1w!blc1hbst@lqu^#dp0i!z*)ljkx^z6j63wu+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', cast=bool)
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
